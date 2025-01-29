8b553df133e8e92cbcd531ba29d18986522bcf34
HarDhr <hardhr@outlook.com>
2025-01-29 13:15:17 +0700

    7th complete html css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Ini JavaScript
+
+function downloadResult() {
+    const newTab = window.open('', '_blank');
+    newTab.document.write('<h1>Hasil BMI Anda</h1><p>Download fitur masih dalam pengembangan.</p>');
+}
+
+function openConsultation(url) {
+    window.open(url, '_blank');
+}
